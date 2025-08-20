ce1f5720827073c8f7e1d570c33922c3db50938d
Rayyan <mmohammedrayyan0808@gmail.com>
2025-08-20 23:23:58 +0530

    Initilize Clover 3.x

--- clover_sweet.mk
+++ clover_sweet.mk
@@ -12,10 +12,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Clover stuff.
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Clover Project Flags
+CLOVER_BUILDTYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+USE_PIXEL_CHARGER := true
+
+PRODUCT_NAME := clover_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
